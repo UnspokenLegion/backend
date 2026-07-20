4cc91d581bf0db038c27f9c10ec68a8715035cf3
Stygixn <l756ouissoto@gmail.com>
2026-07-19 18:24:48 -0600

    Delete src directory

--- src/main/java/com/moneysaver/backend/repositories/BudgetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.moneysaver.backend.repositories;
-
-import com.moneysaver.backend.models.Budget;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BudgetRepository extends JpaRepository<Budget, Integer> {
-}
